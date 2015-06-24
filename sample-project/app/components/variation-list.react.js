0fee96023d7053f8a620994471e97ced78111a8c
Christian Kuehl <kuehlchristian@icloud.com>
2015-06-24 21:54:04 +0200

    Show categories.

--- sample-project/app/components/variation-list.react.js
+++ sample-project/app/components/variation-list.react.js
@@ -1,15 +1,20 @@
 var React = require('react');
-var Variation = require('../../app/components/variation.react.js');
+var VariationCategory = require('../../app/components/variation-category.react.js');
 
 class VariationList extends React.Component {
   render(){
+    var categories = this.props.categories.map((category) => {
+      var variations = this.props.variations
+        .filter((variation) => {
+          return variation.category === category.id;
+        });
+      return <VariationCategory category={category} variations={variations} />
+    });
+
     return <div>
-            These variations are available:
-            <Variation />
-            <Variation />
-            <Variation />
+            <h2>Diese Variationen sind verfügbar:</h2>
+            {categories}
           </div>;
   }
 };
-
 module.exports = VariationList;
