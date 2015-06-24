8aab0d92562a277925ade135257c38a180259e76
Christian Kuehl <kuehlchristian@icloud.com>
2015-06-24 18:06:10 +0200

    Add jsdom bootstrapper, extend variation test.

--- sample-project/tests/variation.test.js
+++ sample-project/tests/variation.test.js
@@ -1,10 +1,18 @@
 var chai = require('chai');
 var expect = chai.expect;
 var React = require('react/addons');
+var DOM = require('../bootstrap-jsdom.js');
+var renderTarget, component;
 
-describe('variations component', () => {  
-  it('should not be null', () => {
-    var component = require('../app/components/variation.react.js');
+describe('variations component', () => {
+  beforeEach(() => {
+    var ComponentClass = require('../app/components/variation.react.js');
+    renderTarget = DOM.document.getElementsByTagName('body')[0];
+    var renderedComponent = React.createElement(ComponentClass, renderTarget);
+    component = renderedComponent;
+  });
+
+  it('should be able to render', () => {
     expect(component).not.to.equal(null);
   });
 });
