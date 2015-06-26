705c84c37258e0ed2ac7499b8bba3af68e715ee7
chrkhl <kuehlchristian@icloud.com>
2015-06-26 10:35:17 +0200

    Fix main.js.

--- sample-project/app/main.js
+++ sample-project/app/main.js
@@ -5,9 +5,8 @@ var App = require('../app/components/app.react.js');
 var DataAccess = require('../app/data-access.js');
 
 
+var dataAccess = new DataAccess();
 var actionCreator = new ActionCreator(dataAccess);
 var store = new Store(actionCreator);
-var dataAccess = new DataAccess();
-
 
 React.render(<App store={store} actionCreator={actionCreator} />, document.body);
--- sample-project/dist/bundle.js
+++ sample-project/dist/bundle.js
@@ -371,9 +371,9 @@ var ActionCreator = require('../app/action-creator.js');
 var App = require('../app/components/app.react.js');
 var DataAccess = require('../app/data-access.js');
 
+var dataAccess = new DataAccess();
 var actionCreator = new ActionCreator(dataAccess);
 var store = new Store(actionCreator);
-var dataAccess = new DataAccess();
 
 React.render(React.createElement(App, { store: store, actionCreator: actionCreator }), document.body);
 
