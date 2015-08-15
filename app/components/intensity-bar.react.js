f437b29fd1ae80278f5b192f2cd994f7b050155c
René Viering <rene.viering@gmail.com>
2015-08-15 17:41:28 +0200

    Fix eslint errors.

--- app/components/intensity-bar.react.js
+++ app/components/intensity-bar.react.js
@@ -1,19 +1,20 @@
 var React = require('react');
 
+
 class IntensityBar extends React.Component {
-  render(){
-    var dots = [];
-    var className = 'dot-on';
-    for(var i = 0; i < 12; i++){
-      if(i >= this.props.intensity) {
-        className = 'dot-off';
-      }
-      dots.push(<span className={className}></span>);
-    };
-    return <div className="intensity-bar">
-            {dots}
-           </div>;
-  }
-};
+    render(){
+        var dots = [];
+        var className = 'dot-on';
+        for(var i = 0; i < 12; i++){
+            if(i >= this.props.intensity) {
+                className = 'dot-off';
+            }
+            dots.push(<span className={className}></span>);
+        }
+        return <div className="intensity-bar">
+        {dots}
+        </div>;
+    }
+}
 
 module.exports = IntensityBar;
