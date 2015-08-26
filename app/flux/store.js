966347db0a40aa3b5c67caa6400d8ba3cb827e7e
Christian Kuehl <kuehlchristian@icloud.com>
2015-08-26 22:01:14 +0200

    addChangeListener returns function to deregister listener.

--- app/flux/store.js
+++ app/flux/store.js
@@ -1,20 +1,31 @@
+function removeChangeListenerWithId(listenerId, eventIdentifier, store) {
+  store.listeners[eventIdentifier] = store.listeners[eventIdentifier]
+    .filter((listener) => { return listener.id !== listenerId; });
+};
+
 class Store {
   constructor() {
     this.listeners = {};
+    this.highestCallbackId = 0;
   }
 
   addChangeListener(eventIdentifier, callback) {
+    var callbackWithId = { callback: callback, id: this.highestCallbackId };
+    this.highestCallbackId++;
+
     if(!this.listeners[eventIdentifier]) {
       this.listeners[eventIdentifier] = [];
     }
-    this.listeners[eventIdentifier].push(callback);
+
+    this.listeners[eventIdentifier].push(callbackWithId);
+    return () => { removeChangeListenerWithId(callbackWithId.id, eventIdentifier, this)};
   }
 
   emitChange(eventIdentifier) {
     var listenersForEvent = this.listeners[eventIdentifier];
     if(listenersForEvent) {
       listenersForEvent.forEach((listener) => {
-        listener();
+        listener.callback();
       })
     }
   }
