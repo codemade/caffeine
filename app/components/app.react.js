ddf4cd38fb1a9eaef99d6da30f51c4934b70487b
Christian Kuehl <kuehlchristian@icloud.com>
2015-08-26 22:08:11 +0200

    Deregister change listener when component will unmount.

--- app/components/app.react.js
+++ app/components/app.react.js
@@ -19,9 +19,13 @@ class App extends React.Component {
     );
   }
 
-  componentWillMount(){
-    this.props.store.addChangeListener('changed', this.handleDataChanged.bind(this));
+  componentWillMount() {
+    this.deregisterChangeListener = this.props.store.addChangeListener('changed', this.handleDataChanged.bind(this));
     this.props.actionCreator.initialize();
+  }
+
+  componentWillUnmount() {
+    this.deregisterChangeListener();
   }
 
   filterByIntensity(intensity) {
