c9bcdfa31fa9fbdef879d9d89ae92d7b764957ff
revrng <rene.viering@gmail.com>
2015-09-03 10:07:29 +0200

    Add shopping-cart route with handler.

--- app/components/app.react.js
+++ app/components/app.react.js
@@ -1,17 +1,23 @@
 let React = require('react');
 let RouterMixin = require('react-mini-router').RouterMixin;
 let ArticlesControllerView = require('./articles-controller-view.react.js');
+let ShoppingCartControllerView = require('./shopping-cart-controller-view.react.js');
 
 const App = React.createClass({
   mixins: [RouterMixin],
   routes: {
-    '/': 'articles'
+    '/': 'articles',
+    '/shopping-cart': 'shoppingCart'
   },
   render() {
-    return this.renderCurrentRoute();
+    var currentRoute = this.renderCurrentRoute();
+    return currentRoute;
   },
   articles() {
     return <ArticlesControllerView store={this.props.store} actionCreator={this.props.actionCreator} />;
+  },
+  shoppingCart() {
+    return <ShoppingCartControllerView/>;
   }
 });
 
