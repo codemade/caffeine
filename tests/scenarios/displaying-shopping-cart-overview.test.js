d84872699ab43a99dbffd0698feee0a2738f647d
Christian Kühl <kuehlchristian@icloud.com>
2015-09-25 12:45:05 +0200

    Make article amounts in specs a little bit more realistic.

--- tests/scenarios/displaying-shopping-cart-overview.test.js
+++ tests/scenarios/displaying-shopping-cart-overview.test.js
@@ -58,42 +58,42 @@ describe('Displaying the shopping cart overview', () => {
 
   describe('when two different articles added to shopping cart', () => {
     beforeEach(() => {
-      actionCreator.addArticleToShoppingCart(3, 5);
-      actionCreator.addArticleToShoppingCart(4, 3);
+      actionCreator.addArticleToShoppingCart(3, 20);
+      actionCreator.addArticleToShoppingCart(4, 30);
     });
 
     it('should display name, amount and price of first article', () => {
       let shoppingCartItem = TestUtils.scryRenderedDOMComponentsWithClass(renderedComponent, 'shopping-cart-item')[0].getDOMNode();
       expect(shoppingCartItem.children[0].textContent).to.equal('first article');
       expect(shoppingCartItem.children[1].textContent).to.equal('0.42');
-      expect(shoppingCartItem.children[2].children[0].textContent).to.equal('5');
-      expect(shoppingCartItem.children[3].textContent).to.equal('2.1');
+      expect(shoppingCartItem.children[2].children[0].textContent).to.equal('20');
+      expect(shoppingCartItem.children[3].textContent).to.equal('8.4');
     });
 
     it('should display name, amount and price of second article', () => {
       let shoppingCartItem = TestUtils.scryRenderedDOMComponentsWithClass(renderedComponent, 'shopping-cart-item')[1].getDOMNode();
       expect(shoppingCartItem.children[0].textContent).to.equal('second article');
       expect(shoppingCartItem.children[1].textContent).to.equal('0.38');
-      expect(shoppingCartItem.children[2].children[0].textContent).to.equal('3');
-      expect(shoppingCartItem.children[3].textContent).to.equal('1.14');
+      expect(shoppingCartItem.children[2].children[0].textContent).to.equal('30');
+      expect(shoppingCartItem.children[3].textContent).to.equal('11.4');
     });
 
     it('should display footer with total amount', () => {
       let shoppingCartFooter = TestUtils.scryRenderedDOMComponentsWithClass(renderedComponent, 'shopping-cart-footer')[0].getDOMNode();
-      expect(shoppingCartFooter.children[2].textContent).to.equal('8');
+      expect(shoppingCartFooter.children[2].textContent).to.equal('50');
     });
 
     it('should display footer with total price', () => {
       let shoppingCartFooter = TestUtils.scryRenderedDOMComponentsWithClass(renderedComponent, 'shopping-cart-footer')[0].getDOMNode();
-      expect(shoppingCartFooter.children[3].textContent).to.equal('3.24');
+      expect(shoppingCartFooter.children[3].textContent).to.equal('19.8');
     });
 
-    describe('Clicking the + button of an article', () => {
+    describe('Clicking the add button of an article', () => {
       it('should increase article amount by 10', () => {
         let shoppingCartItem = TestUtils.scryRenderedDOMComponentsWithClass(renderedComponent, 'shopping-cart-item')[0].getDOMNode();
         let addButton = shoppingCartItem.children[2].children[1];
         React.addons.TestUtils.Simulate.click(addButton);
-        expect(shoppingCartItem.children[2].children[0].textContent).to.equal('15');
+        expect(shoppingCartItem.children[2].children[0].textContent).to.equal('30');
       });
     });
   });
