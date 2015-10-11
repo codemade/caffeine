6ba64769603b100cd5fae4734ca2c4dcd3f98736
René Viering <rene.viering@gmail.com>
2015-10-11 18:14:57 +0200

    Add navigation to shopping-cart view, enable possibility to navigate back to articles overview.

--- app/components/shopping-cart-controller-view.react.js
+++ app/components/shopping-cart-controller-view.react.js
@@ -1,5 +1,6 @@
 let React = require('react');
 let ShoppingCartItem = require('./shopping-cart-item.react.js');
+let Navigation = require('./navigation.react.js');
 
 class ShoppingCartControllerView extends React.Component {
   constructor(props) {
@@ -26,36 +27,43 @@ class ShoppingCartControllerView extends React.Component {
   }
 
   render() {
-    let items = this.state.shoppingCartContent.items.map((item) => {
-      return <ShoppingCartItem key={item.id} article={item} actionCreator={this.props.actionCreator} />;
-    });
+    // let items = this.state.shoppingCartContent.items.map((item) => {
+    //   return <ShoppingCartItem key={item.id} article={item} actionCreator={this.props.actionCreator} />;
+    // });
 
-    let header = <div className='shopping-cart-header'>
-      <div>Artikel</div>
-      <div>Einzelpreis</div>
-      <div>Anzahl</div>
-      <div>Gesamtpreis</div>
-    </div>;
+    // let header = <div className='shopping-cart-header'>
+    //   <div>Artikel</div>
+    //   <div>Einzelpreis</div>
+    //   <div>Anzahl</div>
+    //   <div>Gesamtpreis</div>
+    // </div>;
 
-    let footer = <div className='shopping-cart-footer'>
-        <div></div>
-        <div></div>
-        <div>{this.state.shoppingCartContent.totalAmount}</div>
-        <div>{this.state.shoppingCartContent.totalPrice / 100}</div>
-      </div>;
+    // let footer = <div className='shopping-cart-footer'>
+    //     <div></div>
+    //     <div></div>
+    //     <div>{this.state.shoppingCartContent.totalAmount}</div>
+    //     <div>{this.state.shoppingCartContent.totalPrice / 100}</div>
+    //   </div>;
 
-    let warning = this.state.shoppingCartContent.packagingSizeInvalid
-      ? <div className='shopping-cart-warning'>Gesamtmenge muss ein Vielfaches von 50 sein!</div>
-      : '';
+    // let warning = this.state.shoppingCartContent.packagingSizeInvalid
+    //   ? <div className='shopping-cart-warning'>Gesamtmenge muss ein Vielfaches von 50 sein!</div>
+    //   : '';
 
-    return <div className='shopping-cart'>
-      <h1>Shopping-Cart-View</h1>
-      <div className='shopping-cart-content'>
-        {header}
-        {items}
-        {footer}
+    // return <div className='shopping-cart'>
+    //   <h1>Shopping-Cart-View</h1>
+    //   <div className='shopping-cart-content'>
+    //     {header}
+    //     {items}
+    //     {footer}
+    //   </div>
+    //   {warning}
+    // </div>;
+
+    return <div>
+      <Navigation/>
+      <div className="container contentWrapper">
+        <a href="#"><i className="fa fa-chevron-left"></i> Back to articles overview</a>
       </div>
-      {warning}
     </div>;
   }
 }
