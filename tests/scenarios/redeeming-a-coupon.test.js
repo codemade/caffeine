a2a8720426b7b2f0cff95fbe8ac0a788c838d181
Christian Kühl <kuehlchristian@icloud.com>
2015-10-18 16:39:50 +0200

    Do not show coupon input when no articles in shopping cart.

--- tests/scenarios/redeeming-a-coupon.test.js
+++ tests/scenarios/redeeming-a-coupon.test.js
@@ -5,7 +5,7 @@ let expect = require('chai').expect;
 let StorageFake = require('../flux/storage-fake.js');
 let TestUtils = require('react-addons-test-utils');
 
-let renderTarget, renderedComponent, shoppingCartOverview;
+let renderTarget, renderedComponent, shoppingCartOverview, actionCreator;
 
 describe('redeeming a coupon', () => {
   let categories = [
@@ -31,14 +31,12 @@ describe('redeeming a coupon', () => {
     };
 
     let ActionCreator = require('../../app/action-creator.js');
-    let actionCreator = new ActionCreator(dataAccess);
+    actionCreator = new ActionCreator(dataAccess);
     let ArticleStore = require('../../app/article-store.js');
     let store = new ArticleStore(new StorageFake());
     renderedComponent = ReactDOM.render(<ComponentClass actionCreator={actionCreator} store={store}/>, renderTarget);
     // initialize action creator, to fetch article data from server etc.
     actionCreator.initialize();
-    actionCreator.addArticleToShoppingCart(3, 20);
-    actionCreator.addArticleToShoppingCart(4, 30);
   });
 
   afterEach(() => {
@@ -46,56 +44,65 @@ describe('redeeming a coupon', () => {
     renderedComponent = null;
   });
 
-  describe('with an invalid coupon code', () => {
-    beforeEach(() => {
-      let couponCodeInput = renderTarget.querySelector('.shoppingCart__couponInput__couponCode');
-      let redeemCouponButton = renderTarget.querySelector('.shoppingCart__couponInput__redeemCoupon');
-      couponCodeInput.value = 'invalid coupon code';
-      TestUtils.Simulate.click(redeemCouponButton);
-    });
-
-    it('should show a warning', () => {
-      let couponCodeWarning = renderTarget.querySelector('.shoppingCart__couponCodeWarning');
-      expect(couponCodeWarning).not.to.equal(null);
-    });
-
-    it('should not show the coupon discount price', () => {
-      let couponDiscountRow = renderTarget.querySelector('.shoppingCart__footer__couponDiscount');
-      expect(couponDiscountRow).to.equal(null);
-    });
-
-    it('should not show the reduced total price', () => {
-      let reducedTotalPriceRow = renderTarget.querySelector('.shoppingCart__footer__reducedTotalPrice');
-      expect(reducedTotalPriceRow).to.equal(null);
-    });
-  });
-
-  describe('with a valid coupon code', () => {
-    beforeEach(() => {
-      let couponCodeInput = renderTarget.querySelector('.shoppingCart__couponInput__couponCode');
-      let redeemCouponButton = renderTarget.querySelector('.shoppingCart__couponInput__redeemCoupon');
-      couponCodeInput.value = 'wmks-09-11-15';
-      TestUtils.Simulate.click(redeemCouponButton);
-    });
-
+  describe('with no articles in shopping cart', () => {
     it('should not show coupon input', () => {
       let couponCodeInput = renderTarget.querySelector('.shoppingCart__couponInput');
       expect(couponCodeInput).to.equal(null);
     });
+  });
 
-    it('should not show a warning', () => {
-      let couponCodeWarning = renderTarget.querySelector('.shoppingCart__couponCodeWarning');
-      expect(couponCodeWarning).to.equal(null);
+  describe('with articles in shopping cart', () => {
+    beforeEach(() => {
+      actionCreator.addArticleToShoppingCart(3, 20);
+      actionCreator.addArticleToShoppingCart(4, 30);
     });
 
-    it('should show the coupon discount price', () => {
-      let couponDiscount = renderTarget.querySelector('.shoppingCart__footer__couponDiscount td:last-child');
-      expect(couponDiscount.textContent).to.equal('- 2.97 €');
+    describe('with an invalid coupon code', () => {
+      beforeEach(() => {
+        let couponCodeInput = renderTarget.querySelector('.shoppingCart__couponInput__couponCode');
+        let redeemCouponButton = renderTarget.querySelector('.shoppingCart__couponInput__redeemCoupon');
+        couponCodeInput.value = 'invalid coupon code';
+        TestUtils.Simulate.click(redeemCouponButton);
+      });
+
+      it('should show a warning', () => {
+        let couponCodeWarning = renderTarget.querySelector('.shoppingCart__couponCodeWarning');
+        expect(couponCodeWarning).not.to.equal(null);
+      });
+
+      it('should not show the coupon discount price', () => {
+        let couponDiscountRow = renderTarget.querySelector('.shoppingCart__footer__couponDiscount');
+        expect(couponDiscountRow).to.equal(null);
+      });
+
+      it('should not show the reduced total price', () => {
+        let reducedTotalPriceRow = renderTarget.querySelector('.shoppingCart__footer__reducedTotalPrice');
+        expect(reducedTotalPriceRow).to.equal(null);
+      });
     });
 
-    it('should show the reduced total price', () => {
-      let reducedTotalPrice = renderTarget.querySelector('.shoppingCart__footer__reducedTotalPrice td:last-child');
-      expect(reducedTotalPrice.textContent).to.equal('16.83 €');
+    describe('with a valid coupon code', () => {
+      beforeEach(() => {
+        let couponCodeInput = renderTarget.querySelector('.shoppingCart__couponInput__couponCode');
+        let redeemCouponButton = renderTarget.querySelector('.shoppingCart__couponInput__redeemCoupon');
+        couponCodeInput.value = 'wmks-09-11-15';
+        TestUtils.Simulate.click(redeemCouponButton);
+      });
+
+      it('should not show a warning', () => {
+        let couponCodeWarning = renderTarget.querySelector('.shoppingCart__couponCodeWarning');
+        expect(couponCodeWarning).to.equal(null);
+      });
+
+      it('should show the coupon discount price', () => {
+        let couponDiscount = renderTarget.querySelector('.shoppingCart__footer__couponDiscount td:last-child');
+        expect(couponDiscount.textContent).to.equal('- 2.97 €');
+      });
+
+      it('should show the reduced total price', () => {
+        let reducedTotalPrice = renderTarget.querySelector('.shoppingCart__footer__reducedTotalPrice td:last-child');
+        expect(reducedTotalPrice.textContent).to.equal('16.83 €');
+      });
     });
   });
 });
